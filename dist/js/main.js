6bd29b4da282fec04c64d3649f204362dac3511e
Steven Nagle <sanagle@ymail.com>
2019-04-11 20:48:45 -0400

    update _main.scss translate func

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,6 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-
 const navItems = document.querySelectorAll('nav-item');
 
 //Set intial state of the Menu
@@ -22,24 +21,14 @@ function toggleMenu(){
 
 		//Set menu state
 		showMenu = true;
-		
-
 	}
-	//Close menu
-	else {
-		
+	else{		
 		menuBtn.classList.remove('close');
 		menu.classList.remove('show');
 		menuNav.classList.remove('show');
 		menuBranding.classList.remove('show');
 		navItems.forEach(item => item.classList.remove('show'));
-
 		//Set menu state
 		showMenu = false;
-
-
 	}
-
-
 }
-
